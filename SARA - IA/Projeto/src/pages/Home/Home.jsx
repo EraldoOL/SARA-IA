0c62e8ec9635092e6731f37076fe1009097cc97b
EraldoOL <eo80797@gmail.com>
2025-01-02 15:18:22 -0300

    update Home.jsx

--- SARA - IA/Projeto/src/pages/Home/Home.jsx
+++ SARA - IA/Projeto/src/pages/Home/Home.jsx
@@ -53,6 +53,8 @@ export function Home() {
       systemContent = "A inspiração para este chatbot foi uma amiga do meu Desenvolvedor Eraldo chamada Sara, que conheceu no ensino médio.";
     } else if (userInput.toLowerCase().includes("desenvolvedor")) {
       systemContent = "Eu fui desenvolvido por Eraldo Oliveira, um programador fullstack de 19 anos, ele tem quase 2 anos de experiência na área e é especializado em sistema web front/back e estou na versão 1.8.1";
+    } else if (userInput.toLowerCase().includes("sobre eraldo")) {
+      systemContent = "Ele é bem estudioso, ja fez varios sites e sistemas, ele é um dos programadores mais inteligente do universo";
     } else {
       systemContent = "Você está falando com o chatbot Sara, desenvolvido por Eraldo Oliveira. Eu sou uma IA pronta para responder suas perguntas e ajudar no que você precisar.";
     }
